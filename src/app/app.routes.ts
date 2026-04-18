9b304f3c910f6a11359ab4c80aca5fc97e74ce8d
heber leandro <heberleandroluz@gmail.com>
2026-04-18 18:21:10 -0300

    feat: first github pages deploy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
-import { UserBoardsComponent } from './modules/boards/pages/user-boards/user-boards.component';
-import { PageNotFoundComponent } from './modules/page-not-found/page-not-found.component';
-import { LoginComponent } from './modules/landpage/pages/login/login.component';
 
 export const routes: Routes = [
-  { path: '', component: LoginComponent },
-  { path: 'u/:username/boards', component: UserBoardsComponent },
-  { path: '**', component:  PageNotFoundComponent},
+  { 
+    path: '', 
+    loadComponent: () => import('./modules/landpage/pages/login/login.component').then(m => m.LoginComponent) 
+  },
+  { 
+    path: 'u/:username/boards', 
+    loadComponent: () => import('./modules/boards/pages/user-boards/user-boards.component').then(m => m.UserBoardsComponent)
+  },
+  { 
+    path: '**', 
+    loadComponent: () => import('./modules/page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent)
+  },
 ];
