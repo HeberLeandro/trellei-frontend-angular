8ca9a329833bb4d3d4bff7aecd4a03fa077d8c16
heber leandro <heberleandroluz@gmail.com>
2026-06-11 22:25:16 -0300

    feat: adds signup route on login form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,10 @@ export const routes: Routes = [
     loadComponent: () => import('./modules/landpage/pages/login/login.component').then(m => m.LoginComponent) 
   },
   { 
+    path: 'signup', 
+    loadComponent: () => import('./modules/landpage/pages/signup/signup.component').then(m => m.SignupComponent)
+  },
+  { 
     path: 'u/:username/boards', 
     loadComponent: () => import('./modules/boards/pages/user-boards/user-boards.component').then(m => m.UserBoardsComponent)
   },
--- src/app/modules/landpage/pages/login/login.component.ts
+++ src/app/modules/landpage/pages/login/login.component.ts
@@ -1,19 +1,27 @@
 import { Component, signal } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { ImageModule } from 'primeng/image';
 import { HeaderBarComponent } from '../../../../shared-module/header-bar/header-bar.component';
 import { ButtonModule } from 'primeng/button';
+import { MenuModule } from "primeng/menu";
+import { CheckboxModule } from 'primeng/checkbox';
+
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, ImageModule, HeaderBarComponent, ButtonModule],
+  imports: [RouterModule, FormsModule, ImageModule, HeaderBarComponent, ButtonModule, MenuModule, CheckboxModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  value = signal('Teste');
+  rememberMe = signal(false);
+  
+  login() : void {
+    console.log('Login button clicked');
+  }
 }
 
 
